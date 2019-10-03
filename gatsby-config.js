f679067f1121fa9525881a0602e5ca078cafa7e0
Jayson Yan <jj2yan@uwaterloo.ca>
2019-10-02 22:29:36 -0400

    add google analytics and housekeeping

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,11 @@ module.exports = {
         icon: `src/images/logo.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-141054868-1',
+      },
+    },
   ],
 }
