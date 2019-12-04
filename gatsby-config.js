430b6139b9a4a4163368ce315108312f9577bf27
Jayson Yan <jj2yan@uwaterloo.ca>
2019-12-03 20:39:43 -0500

    fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
-    `gastby-plugin-sitemap`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
