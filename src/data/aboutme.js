18b2dd1e4a6d47de3c4ff5b81a9e9b6ce6ebf67d
Jayson Yan <jj2yan@uwaterloo.ca>
2020-06-22 23:17:58 -0400

    update bio

--- src/data/aboutme.js
+++ src/data/aboutme.js
@@ -1,5 +1,5 @@
 const AboutData = {
-    body: "Hey There! 😄 \n\nI'm Jayson, a Software Engineering student at the University of Waterloo. \n\nOutside of work and school you'll probably find me snowboarding, snacking, or playing (losing at) smash bros.\n\nCurrently I'm completing my 3A term at Waterloo and looking for Summer 2020 internship opportunities!"
+    body: "Hey There! 😄 \n\nI'm Jayson, a Software Engineering student at the University of Waterloo. \n\nOutside of work and school you'll probably find me snowboarding, snacking, or playing (losing at) smash bros.\n\n Currently I'm interning at Wish working on the core infrastructure team."
 }
 
 export default AboutData
