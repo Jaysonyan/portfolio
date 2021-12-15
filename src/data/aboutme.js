cda17b84c4626ffa4f5daed2b0d5ed78cff671cb
Jayson Yan <jaysonjyan@gmail.com>
2021-12-15 00:58:26 -0500

    Update aboutme.js

--- src/data/aboutme.js
+++ src/data/aboutme.js
@@ -1,5 +1,5 @@
 const AboutData = {
-    body: "Hey There! 😄 \n\nI'm Jayson, a Software Engineering student at the University of Waterloo. \n\nOutside of work and school you'll probably find me snowboarding, snacking, or playing (losing at) smash bros.\n\n This winter I'll be joining Splunk as an intern on the Developer Platforms team."
+    body: "Hey There! 😄 \n\nI'm Jayson, a Software Engineering student at the University of Waterloo. \n\nOutside of work and school you'll probably find me snowboarding, snacking, or playing (losing at) smash bros."
 }
 
 export default AboutData
